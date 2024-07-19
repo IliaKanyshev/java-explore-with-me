be3b90ec0aee2aacdaeea3a7ef30c819fb870ae9
AqpP <aqpporig@gmail.com>
2024-07-19 21:11:03 +0300

    ewm v1

--- stat-service/stat-server/src/main/java/ru/practicum/server/exception/ErrorHandler.java
+++ stat-service/stat-server/src/main/java/ru/practicum/server/exception/ErrorHandler.java
@@ -1,6 +1,8 @@
 package ru.practicum.server.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -18,5 +20,17 @@ public class ErrorHandler {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleThrowableExceptions(final Exception e) {
         return new ErrorResponse("Internal server error 500.");
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
+        return new ErrorResponse("Method Argument Validation exception.");
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMissingRequestParameterException(final MissingServletRequestParameterException e) {
+        return new ErrorResponse("Parameter missing.");
     }
 }
